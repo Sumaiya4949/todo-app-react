55e781b2de855474f09fc875ee40321f45fc225a
sumaiya4949 <sumaiya15-4949@diu.edu.bd>
2022-05-07 13:53:10 +0200

    Register using api

--- src/pages/RegistrationForm.tsx
+++ src/pages/RegistrationForm.tsx
@@ -1,22 +1,43 @@
 import { Form, Input, Button, Typography, notification } from "antd";
+import axios from "axios";
+import { SHA3 } from "sha3";
 
 const { Title } = Typography;
 
 export const RegistrationForm = () => {
-  const onFinish = (values: any) => {
-    notification.success({
-      message: `Registration successfull`,
-      description: "Taking you back to login page",
-      placement: "top",
-    });
-  };
-
   const onFinishFailed = () => {
     notification.error({
       message: `Registration failed`,
       description: "Please try again",
       placement: "top",
     });
+  };
+
+  const onFinish = async (values: any) => {
+    const hash = new SHA3(512);
+    hash.update(values.password);
+
+    try {
+      const { data } = await axios.put("/auth/register", {
+        email: values.email,
+        fullname: values.fullname,
+        passwordHash: hash.digest("hex"),
+      });
+
+      if (data.success) {
+        notification.success({
+          message: `Registration successfull`,
+          description: "Taking you back to login page",
+          placement: "top",
+        });
+      }
+    } catch (error: any) {
+      notification.error({
+        message: `Registration failed`,
+        description: `${error?.response?.data?.message}. Please try again.`,
+        placement: "top",
+      });
+    }
   };
 
   return (
@@ -35,7 +56,8 @@ export const RegistrationForm = () => {
       onFinishFailed={onFinishFailed}
       autoComplete="off"
     >
-      <Title level={3}>Please register yourself to login</Title>
+      <Title level={4}>Please register yourself to use this app</Title>
+      <br />
       <Form.Item
         name={"email"}
         label="Email"
