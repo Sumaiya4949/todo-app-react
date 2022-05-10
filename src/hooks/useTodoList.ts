d0f32bfc96088e27795af2b48fd32c9ac6e3d452
sumaiya4949 <sumaiya15-4949@diu.edu.bd>
2022-05-10 15:36:40 +0200

    Refactor

--- src/hooks/useTodoList.ts
+++ src/hooks/useTodoList.ts
@@ -6,21 +6,7 @@ import { TodoType } from "../types";
  * @returns {object} Todo list and some functions to add, remove and check/uncheck funtionality
  */
 export const useTodoList = () => {
-  // Load todos from local storage
-  // const initialTodos: TodoType[] = useMemo(() => {
-  //   return JSON.parse(localStorage.getItem("todos") || "[]");
-  // }, []);
-
   const [myTodos, setMyTodos] = useState<TodoType[]>([]);
-
-  /**
-   * Effect to save todos to local storage when todos change
-   * @description
-   *  - Saves the todos to local storage as JSON string.
-   */
-  // useEffect(() => {
-  //   localStorage.setItem("todos", JSON.stringify(myTodos));
-  // }, [myTodos]);
 
   /**
    * Add new todo to the todo list
