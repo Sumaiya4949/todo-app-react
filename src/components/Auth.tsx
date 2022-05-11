3b1808dd88895099ea0b342c2c64b549b3f47781
sumaiya4949 <sumaiya15-4949@diu.edu.bd>
2022-05-11 13:21:44 +0200

    Memoize and document the functions

--- src/components/Auth.tsx
+++ src/components/Auth.tsx
@@ -1,4 +1,4 @@
-import { createContext, useEffect, useState } from "react";
+import { createContext, useCallback, useEffect, useState } from "react";
 import { User } from "../types";
 import axios from "axios";
 
@@ -33,13 +33,29 @@ export const Auth = (props: PropType) => {
   const [isLoggedIn, setIsLoggedIn] = useState<boolean>(initialLoginState);
   const [authUser, setAuthUser] = useState<User | null>(initialAuthUserState);
 
-  const setLoginStatus = (status: boolean, user: User | null) => {
+  /**
+   * Set user and user's login status to local storage and state
+   * @description
+   *  - Sets login status to local storage and state
+   *  - Sets user to local storage and state
+   * @param {boolean} status Flag to determine if the user is logged in
+   * @param {User} user Information of currently logged user
+   */
+  const setLoginStatus = useCallback((status: boolean, user: User | null) => {
     setIsLoggedIn(status);
     setAuthUser(user);
     localStorage.setItem("isLoggedIn", status.toString());
     localStorage.setItem("authUser", JSON.stringify(user));
-  };
+  }, []);
 
+  /**
+   * Effect to fetch currently logged in user from server
+   * @description
+   *  - Fetches currently logged in user from server
+   *  - Saves login status and auth user information
+   *  - If error occurs
+   *    - Erase saved auth user information
+   */
   useEffect(() => {
     const fethInitialAuthUserFromDb = async () => {
       try {
@@ -51,6 +67,7 @@ export const Auth = (props: PropType) => {
         setLoginStatus(false, null);
       }
     };
+
     fethInitialAuthUserFromDb();
   }, []);
 
